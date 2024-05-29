457b170686e0f36be993f34ccbbcc734d1a3d0c6
zsaralin <zsaralin@gmail.com>
2024-05-29 13:42:14 -0400

    moved 42 inside gloc-be

--- gloc-be/server.js
+++ gloc-be/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 4000;
 const cors = require('cors')
 const {findNearestDescriptors, loadDataIntoMemory} = require('./utils/topDescriptors');
 require('dotenv').config();
-const localFolderPath  = '..\\42\\'// '..\\..\\face_backet\\'
+const localFolderPath  = './42'// '..\\..\\face_backet\\'
 
 app.use(cors())
 app.use(express.json());
